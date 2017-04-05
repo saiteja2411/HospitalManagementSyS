fdbfe6b5b978e05aa616fc38f12688a4f8233d62
SAITEJA <saiteja24nov@hotmail.com>
2017-04-04 20:55:01 -0400

    inheritance added

--- src/main/java/com/model/Patient.java
+++ src/main/java/com/model/Patient.java
@@ -7,20 +7,21 @@ package com.model;
 
 import java.io.Serializable;
 import javax.persistence.CascadeType;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 
 /**
  *
  * @author Saiteja
  */
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Patient implements Serializable {
 
     private static final long serialVersionUID = 1L;
